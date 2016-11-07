ecb116be369aea60d78bd7783951a19707d67b61
mtbsickrider <enriquejosepadilla@gmail.com>
2016-11-06 20:26:59 -0500

    React Router v4 Works.

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,23 @@
 import React, {  PropTypes } from 'react';
 import Header from './common/Header';
-
+import { BrowserRouter } from 'react-router';
 
 class App extends  React.Component {
   render() {
     return (
-      <div className="container-fluid">
-        <Header/>
-        {this.props.children}
-      </div>
+      <BrowserRouter>
+        <div className="container-fluid">
+          <Header/>
+
+        </div>
+      </BrowserRouter>
     );
   }
 }
 
-App.propTypes = {
-  children: PropTypes.object.isRequired
-};
+// App.propTypes = {
+//   children: PropTypes.object.isRequired
+// };
 
 
 export default App;
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,14 +1,22 @@
 import React, { PropTypes} from 'react';
-import { Link, IndexLink} from 'react-router';
+import Link from 'react-router/Link';
+import Home from '../home/HomePage';
+import About from '../about/AboutPage';
+import Courses from '../course/CoursesPage';
+import Match from 'react-router/Match';
 
 const Header = () => {
   return (
     <nav>
-      <IndexLink to="/" activeClassName="active">Home</IndexLink>
+      <Link to="/">Home</Link>
       {" | "}
-      <Link to="/about" activeClassName="active">About</Link>
+      <Link to="/about" >About</Link>
       {" | "}
-      <Link to="/courses" activeClassName="active">Courses</Link>
+      <Link to="/courses">Courses</Link>
+
+      <Match exactly pattern="/" component={Home} />
+      <Match pattern="/about" component={About} />
+      <Match pattern="/courses" component={Courses} />
     </nav>
   );
 };
